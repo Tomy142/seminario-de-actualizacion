61f8fca718980540986ab877d465028f0a09ba7e
Tomy142 <tomaslema.01@gmail.com>
2025-09-05 15:39:09 -0300

    Unit4 act1

---
+++ unidad-4/actividad-1/main.js
@@ -0,0 +1,9 @@
+import { XMLHttpRequestExample } from "./HtmlTextAreaWC.js";
+
+
+function main(){
+    let test = new XMLHttpRequestExample();
+    document.body.appendChild(test);
+}
+
+window.onload = main;
